4775abc18de946f6cde07ecd2f66c3f8b0a4ba5a
ohju96 <ohju96@naver.com>
2024-01-07 18:09:25 +0900

    :seedling: set : 프로젝트 기본 파일 추가

---
+++ service/src/main/java/com/pc/service/CheckController.java
@@ -0,0 +1,15 @@
+package com.pc.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CheckController {
+
+    @GetMapping
+    private String DefaultStringApi() {
+        return "Hello Puppy connect service!";
+    }
+}
